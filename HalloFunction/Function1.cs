f8328b87a9fddab78cf6656e6e2feca29770f332
Wojciech Borowiecki <wojciech.borowiecki@avanade.com>
2022-11-08 10:28:07 -0500

    Initial commit

---
+++ HalloFunction/Function1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HalloFunction
+{
+    public static class Function1
+    {
+        [FunctionName("isit")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string responseMessage = "It's alive!";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
